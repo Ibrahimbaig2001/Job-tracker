28bae8911cc396c1b16cf9b26ee08254e1057098
Ibrahim baig Mirza <ibrahimbaigmirza@Ibrahims-MacBook-Air.local>
2026-02-04 00:41:49 +0530

    started working on the project using next.js and made a navbar which needs so more work to be done

---
+++ job-tracker/app/components/Navbar.js
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='px-1 pt-2 mx-auto  '>I am a Navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
