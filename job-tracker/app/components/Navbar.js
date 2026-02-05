e43814fa0c7470aff0d3d1a5c2a3f4d095ceac2f
Ibrahim baig Mirza <ibrahimbaigmirza@Ibrahims-MacBook-Air.local>
2026-02-05 23:55:31 +0530

    added search bar and a search button along with some styles to it

--- job-tracker/app/components/Navbar.js
+++ job-tracker/app/components/Navbar.js
@@ -7,9 +7,9 @@ const Navbar = () => {
     <nav className='h-15 bg-purple-700 flex justify-between px-2 items-center text-white font-bold'>
       <span className='text-lg'>Job-Search</span>
       <ul className='flex gap-3'>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/sign In">Sign In</a></li>
+        <li><a href="/" className='hover:bg-purple-800'>Home</a></li>
+        <li><a href="/about" className='hover:bg-purple-800'>About</a></li>
+        <li><a href="/sign In" className='hover:bg-purple-800'>Sign In</a></li>
       </ul>
     </nav>
     </>
--- job-tracker/app/page.js
+++ job-tracker/app/page.js
@@ -4,7 +4,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    <h1>Hello World</h1>
+    <div className="flex flex-col items-center justify-center h-15 gap-2 m-2 py-10">
+      <span><input type="text" placeholder="Search Your Required work" className="border border-gray-300 rounded-md w-75"/>
+      <button className="cursor-pointer mx-2 border border-1 px-1 rounded-md h-7 hover:bg-gray-200">Search</button></span>
+    </div>
     </>
   );
 }
