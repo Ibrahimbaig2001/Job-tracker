93e25730b649aac247d570498ffed92a44d8075f
Ibrahim baig Mirza <ibrahimbaigmirza@Ibrahims-MacBook-Air.local>
2026-02-12 12:55:20 +0530

    created a sigin page which renders on a new page

---
+++ job-tracker/app/(auth)/signin/page.js
@@ -0,0 +1,15 @@
+'use client'
+import Image from "next/image";
+
+export default function SignInPage() {
+  return (
+    <>
+   <div>
+    <input type="text" placeholder="Enter Your Email"/>
+    <input type="password" name="password" id="password" placeholder="Enter Your Password" />
+   </div>
+    </>
+  );
+}
+
+
--- job-tracker/app/components/Navbar.js
+++ job-tracker/app/components/Navbar.js
@@ -1,5 +1,6 @@
 'use client'
 import React from 'react'
+import Link from 'next/link'
 
 const Navbar = () => {
   return (
@@ -7,9 +8,9 @@ const Navbar = () => {
     <nav className='h-15 bg-purple-700 flex justify-between px-2 items-center text-white font-bold'>
       <span className='text-lg'>Job-Search</span>
       <ul className='flex gap-3'>
-        <li><a href="/" className='hover:bg-purple-800'>Home</a></li>
-        <li><a href="/about" className='hover:bg-purple-800'>About</a></li>
-        <li><a href="/sign In" className='hover:bg-purple-800'>Sign In</a></li>
+        <li><Link href="/" className='hover:bg-purple-800'>Home</Link></li>
+        <li><Link href="/about" className='hover:bg-purple-800'>About</Link></li>
+        <li><Link href="/signin" className='hover:bg-purple-800'>Sign In</Link></li>
       </ul>
     </nav>
     </>
